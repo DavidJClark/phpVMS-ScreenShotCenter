81a9e94fdb007adeceffe4c7b9acc1ae16b23d5e
DavidJClark <david@noticeabledesigns.com>
2013-02-18 21:58:37 -0500

    Updates for plugin manager compatibility

--- core/common/ScreenshotsData.class.php
+++ core/common/ScreenshotsData.class.php
@@ -13,7 +13,7 @@ class ScreenshotsData extends CodonData{
 
     public function getscreenshots()    {
         $query = "SELECT * 
-                    FROM screenshots
+                    FROM ".TABLE_PREFIX."screenshots
                     WHERE file_approved='1'
                     ORDER BY date_uploaded DESC";
 
@@ -21,7 +21,7 @@ class ScreenshotsData extends CodonData{
     }
 
     public function getpagnated($limit)   {
-        $query = "SELECT * FROM screenshots
+        $query = "SELECT * FROM ".TABLE_PREFIX."screenshots
                     WHERE file_approved='1'
                     ORDER BY date_uploaded DESC
                     $limit";
@@ -31,7 +31,7 @@ class ScreenshotsData extends CodonData{
 
     public function getscreenshot($id)    {
         $query = "SELECT *
-                    FROM screenshots
+                    FROM ".TABLE_PREFIX."screenshots
                     WHERE id='$id'";
 
         return DB::get_row($query);
@@ -40,14 +40,14 @@ class ScreenshotsData extends CodonData{
     public function increase_views($id) {
         $views = self::getscreenshot($id);
         $total = $views->views + 1;
-        $query = "UPDATE screenshots SET views='$total' WHERE id='$id'";
+        $query = "UPDATE ".TABLE_PREFIX."screenshots SET views='$total' WHERE id='$id'";
 
         DB::query($query);
     }
 
     public function check_boost($pid, $ssid)    {
         $query = "SELECT COUNT(`id`) AS `total`
-		FROM screenshots_ratings
+		FROM ".TABLE_PREFIX."screenshots_ratings
                 WHERE ss_id=$ssid
                 AND pilot_id=$pid";
         return DB::get_row($query);
@@ -56,76 +56,76 @@ class ScreenshotsData extends CodonData{
     public function increase_karma($id) {
         $rating = self::getscreenshot($id);
         $total = $rating->rating + 1;
-        $query = "UPDATE screenshots SET rating='$total' WHERE id='$id'";
+        $query = "UPDATE ".TABLE_PREFIX."screenshots SET rating='$total' WHERE id='$id'";
         DB::query($query);
         $pid = Auth::$userinfo->pilotid;
-        $query2 = "INSERT INTO screenshots_ratings (ss_id, pilot_id)
+        $query2 = "INSERT INTO ".TABLE_PREFIX."screenshots_ratings (ss_id, pilot_id)
                     VALUES ('$id', '$pid')";
         DB::query($query2);
     }
 
     public function getscreenshots_toapprove()    {
         $query = "SELECT *
-                    FROM screenshots
+                    FROM ".TABLE_PREFIX."screenshots
                     WHERE file_approved='0'";
 
         return DB::get_results($query);
     }
 
     public function approve_screenshot($id) {
-        $upd = "UPDATE screenshots SET file_approved='1' WHERE id='$id'";
+        $upd = "UPDATE ".TABLE_PREFIX."screenshots SET file_approved='1' WHERE id='$id'";
 
         DB::query($upd);
     }
 
     public function reject_screenshot($id) {
-        $upd = "UPDATE screenshots SET file_approved='2' WHERE id='$id'";
+        $upd = "UPDATE ".TABLE_PREFIX."screenshots SET file_approved='2' WHERE id='$id'";
 
         DB::query($upd);
     }
     
     public function delete_screenshot($id) {
-        $query = "DELETE FROM screenshots
+        $query = "DELETE FROM ".TABLE_PREFIX."screenshots
 		  WHERE id='$id'";
 
         DB::query($query);
     }
 
     public function get_newest_screenshot() {
-        $query = "SELECT id, file_name, file_description, pilot_id, UNIX_TIMESTAMP(date_uploaded) AS date FROM screenshots WHERE file_approved='1' ORDER BY date_uploaded DESC LIMIT 1;";
+        $query = "SELECT id, file_name, file_description, pilot_id, UNIX_TIMESTAMP(date_uploaded) AS date FROM ".TABLE_PREFIX."screenshots WHERE file_approved='1' ORDER BY date_uploaded DESC LIMIT 1;";
 
         return DB::get_row($query);
     }
 
     public function get_random_screenshot() {
-        $query = "SELECT id, file_name, file_description, pilot_id, UNIX_TIMESTAMP(date_uploaded) AS date FROM screenshots WHERE file_approved='1' ORDER BY RAND() LIMIT 1;";
+        $query = "SELECT id, file_name, file_description, pilot_id, UNIX_TIMESTAMP(date_uploaded) AS date FROM ".TABLE_PREFIX."screenshots WHERE file_approved='1' ORDER BY RAND() LIMIT 1;";
 
         return DB::get_row($query);
     }
 
     public function send_message($who_to, $who_from, $subject, $message)  {
-        $sql="INSERT INTO airmail (who_to, who_from, subject, message, date, time, notam)
+        $sql="INSERT INTO ".TABLE_PREFIX."airmail (who_to, who_from, subject, message, date, time, notam)
 		VALUES ('$who_to', '$who_from', '$subject', '$message', NOW(), NOW(), '0')";
 
         DB::query($sql);
     }
 
     public function add_comment($ss_id, $pilot_id, $comment)    {
-        $query ="INSERT INTO screenshots_comments (pilot_id, ss_id, comment)
+        $query ="INSERT INTO ".TABLE_PREFIX."screenshots_comments (pilot_id, ss_id, comment)
                 VALUES ('$pilot_id', '$ss_id', '$comment')";
 
         DB::query($query);
     }
 
     public function get_comments($ss_id)   {
-        $query = "SELECT * FROM screenshots_comments
+        $query = "SELECT * FROM ".TABLE_PREFIX."screenshots_comments
                     WHERE ss_id='$ss_id'";
 
         return DB::get_results($query);
     }
 
     public function get_previous($last) {
-        $query = "SELECT id FROM screenshots
+        $query = "SELECT id FROM ".TABLE_PREFIX."screenshots
                     WHERE id<'$last'
                     AND file_approved='1'
                     ORDER BY id DESC";
@@ -134,7 +134,7 @@ class ScreenshotsData extends CodonData{
     }
 
     public function get_next($last) {
-        $query = "SELECT id FROM screenshots
+        $query = "SELECT id FROM ".TABLE_PREFIX."screenshots
                     WHERE id>'$last'
                     AND file_approved='1'
                     ORDER BY id ASC";
@@ -143,7 +143,7 @@ class ScreenshotsData extends CodonData{
     }
 
     public function get_pilots_newscreenshot($pilot_id) {
-        $query = "SELECT id, file_name, file_description, pilot_id, UNIX_TIMESTAMP(date_uploaded) AS date FROM screenshots WHERE pilot_id='$pilot_id' AND file_approved='1' ORDER BY date_uploaded DESC LIMIT 1;";
+        $query = "SELECT id, file_name, file_description, pilot_id, UNIX_TIMESTAMP(date_uploaded) AS date FROM ".TABLE_PREFIX."screenshots WHERE pilot_id='$pilot_id' AND file_approved='1' ORDER BY date_uploaded DESC LIMIT 1;";
 
         return DB::get_row($query);
     }
--- screenshotcenter.sql
+++ screenshotcenter.sql
@@ -1,31 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 3.2.0.1
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Jul 11, 2010 at 11:08 AM
--- Server version: 5.1.36
--- PHP Version: 5.2.9
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `940`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `screenshots`
---
-
-CREATE TABLE IF NOT EXISTS `screenshots` (
+CREATE TABLE IF NOT EXISTS `phpvms_screenshots` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `file_name` varchar(50) NOT NULL,
   `file_description` varchar(150) NOT NULL,
@@ -37,13 +10,7 @@ CREATE TABLE IF NOT EXISTS `screenshots` (
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `screenshots_comments`
---
-
-CREATE TABLE IF NOT EXISTS `screenshots_comments` (
+CREATE TABLE IF NOT EXISTS `phpvms_screenshots_comments` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `pilot_id` int(11) NOT NULL,
   `ss_id` int(11) NOT NULL,
@@ -51,13 +18,7 @@ CREATE TABLE IF NOT EXISTS `screenshots_comments` (
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `screenshots_ratings`
---
-
-CREATE TABLE IF NOT EXISTS `screenshots_ratings` (
+CREATE TABLE IF NOT EXISTS `phpvms_screenshots_ratings` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `ss_id` int(4) NOT NULL,
   `pilot_id` int(4) NOT NULL,
