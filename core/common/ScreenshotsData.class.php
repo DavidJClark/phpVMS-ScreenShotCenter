79031a16811fdfb1e6dcfbf76f4c2d41945c92f3
DavidJClark <david@noticeabledesigns.com>
2015-02-14 19:28:30 -0500

    php 5.x updates to data class.
    Signed-off-by: DavidJClark <david@noticeabledesigns.com>

--- core/common/ScreenshotsData.class.php
+++ core/common/ScreenshotsData.class.php
@@ -11,7 +11,7 @@
 
 class ScreenshotsData extends CodonData{
 
-    public function getscreenshots()    {
+    public static function getscreenshots()    {
         $query = "SELECT * 
                     FROM ".TABLE_PREFIX."screenshots
                     WHERE file_approved='1'
@@ -20,7 +20,7 @@ class ScreenshotsData extends CodonData{
         return DB::get_results($query);
     }
 
-    public function getpagnated($limit)   {
+    public static function getpagnated($limit)   {
         $query = "SELECT * FROM ".TABLE_PREFIX."screenshots
                     WHERE file_approved='1'
                     ORDER BY date_uploaded DESC
@@ -29,7 +29,7 @@ class ScreenshotsData extends CodonData{
         return DB::get_results($query);
     }
 
-    public function getscreenshot($id)    {
+    public static function getscreenshot($id)    {
         $query = "SELECT *
                     FROM ".TABLE_PREFIX."screenshots
                     WHERE id='$id'";
@@ -37,7 +37,7 @@ class ScreenshotsData extends CodonData{
         return DB::get_row($query);
     }
 
-    public function increase_views($id) {
+    public static function increase_views($id) {
         $views = self::getscreenshot($id);
         $total = $views->views + 1;
         $query = "UPDATE ".TABLE_PREFIX."screenshots SET views='$total' WHERE id='$id'";
@@ -45,7 +45,7 @@ class ScreenshotsData extends CodonData{
         DB::query($query);
     }
 
-    public function check_boost($pid, $ssid)    {
+    public static function check_boost($pid, $ssid)    {
         $query = "SELECT COUNT(`id`) AS `total`
 		FROM ".TABLE_PREFIX."screenshots_ratings
                 WHERE ss_id=$ssid
@@ -53,7 +53,7 @@ class ScreenshotsData extends CodonData{
         return DB::get_row($query);
     }
 
-    public function increase_karma($id) {
+    public static function increase_karma($id) {
         $rating = self::getscreenshot($id);
         $total = $rating->rating + 1;
         $query = "UPDATE ".TABLE_PREFIX."screenshots SET rating='$total' WHERE id='$id'";
@@ -64,7 +64,7 @@ class ScreenshotsData extends CodonData{
         DB::query($query2);
     }
 
-    public function getscreenshots_toapprove()    {
+    public static function getscreenshots_toapprove()    {
         $query = "SELECT *
                     FROM ".TABLE_PREFIX."screenshots
                     WHERE file_approved='0'";
@@ -72,59 +72,59 @@ class ScreenshotsData extends CodonData{
         return DB::get_results($query);
     }
 
-    public function approve_screenshot($id) {
+    public static function approve_screenshot($id) {
         $upd = "UPDATE ".TABLE_PREFIX."screenshots SET file_approved='1' WHERE id='$id'";
 
         DB::query($upd);
     }
 
-    public function reject_screenshot($id) {
+    public static function reject_screenshot($id) {
         $upd = "UPDATE ".TABLE_PREFIX."screenshots SET file_approved='2' WHERE id='$id'";
 
         DB::query($upd);
     }
     
-    public function delete_screenshot($id) {
+    public static function delete_screenshot($id) {
         $query = "DELETE FROM ".TABLE_PREFIX."screenshots
 		  WHERE id='$id'";
 
         DB::query($query);
     }
 
-    public function get_newest_screenshot() {
+    public static function get_newest_screenshot() {
         $query = "SELECT id, file_name, file_description, pilot_id, UNIX_TIMESTAMP(date_uploaded) AS date FROM ".TABLE_PREFIX."screenshots WHERE file_approved='1' ORDER BY date_uploaded DESC LIMIT 1;";
 
         return DB::get_row($query);
     }
 
-    public function get_random_screenshot() {
+    public static function get_random_screenshot() {
         $query = "SELECT id, file_name, file_description, pilot_id, UNIX_TIMESTAMP(date_uploaded) AS date FROM ".TABLE_PREFIX."screenshots WHERE file_approved='1' ORDER BY RAND() LIMIT 1;";
 
         return DB::get_row($query);
     }
 
-    public function send_message($who_to, $who_from, $subject, $message)  {
+    public static function send_message($who_to, $who_from, $subject, $message)  {
         $sql="INSERT INTO ".TABLE_PREFIX."airmail (who_to, who_from, subject, message, date, time, notam)
 		VALUES ('$who_to', '$who_from', '$subject', '$message', NOW(), NOW(), '0')";
 
         DB::query($sql);
     }
 
-    public function add_comment($ss_id, $pilot_id, $comment)    {
+    public static function add_comment($ss_id, $pilot_id, $comment)    {
         $query ="INSERT INTO ".TABLE_PREFIX."screenshots_comments (pilot_id, ss_id, comment)
                 VALUES ('$pilot_id', '$ss_id', '$comment')";
 
         DB::query($query);
     }
 
-    public function get_comments($ss_id)   {
+    public static function get_comments($ss_id)   {
         $query = "SELECT * FROM ".TABLE_PREFIX."screenshots_comments
                     WHERE ss_id='$ss_id'";
 
         return DB::get_results($query);
     }
 
-    public function get_previous($last) {
+    public static function get_previous($last) {
         $query = "SELECT id FROM ".TABLE_PREFIX."screenshots
                     WHERE id<'$last'
                     AND file_approved='1'
@@ -133,7 +133,7 @@ class ScreenshotsData extends CodonData{
         return DB::get_row($query);
     }
 
-    public function get_next($last) {
+    public static function get_next($last) {
         $query = "SELECT id FROM ".TABLE_PREFIX."screenshots
                     WHERE id>'$last'
                     AND file_approved='1'
@@ -142,7 +142,7 @@ class ScreenshotsData extends CodonData{
         return DB::get_row($query);
     }
 
-    public function get_pilots_newscreenshot($pilot_id) {
+    public static function get_pilots_newscreenshot($pilot_id) {
         $query = "SELECT id, file_name, file_description, pilot_id, UNIX_TIMESTAMP(date_uploaded) AS date FROM ".TABLE_PREFIX."screenshots WHERE pilot_id='$pilot_id' AND file_approved='1' ORDER BY date_uploaded DESC LIMIT 1;";
 
         return DB::get_row($query);
